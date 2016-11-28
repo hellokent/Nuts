7991be8710ed317eb363b4c25b52d8f68920f3f7
陈阳 <chenyang@edaijia-staff.cn>
2016-11-28 19:33:37 +0800

    stage 1

---
+++ lib-common/src/main/java/io/demor/nuts/lib/ClusterExecutor.java
@@ -0,0 +1,205 @@
+package io.demor.nuts.lib;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.collect.*;
+import com.google.common.primitives.Ints;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ClusterExecutor extends ThreadPoolExecutor {
+
+
+    private static final String DEFAULT_LOCK = "CLUSTER";
+
+    public final ThreadLocal<String> cAffineThreadLocal = new ThreadLocal<>();
+    protected final ClusterQueue cQueue;
+
+    public ClusterExecutor(final int corePoolSize, final int maximumPoolSize,
+                           final long keepAliveTime, final TimeUnit unit, final RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, new ClusterQueue(), handler);
+        final ClusterThreadFactor factory = new ClusterThreadFactor();
+        setThreadFactory(factory);
+        cQueue = (ClusterQueue) getQueue();
+        cQueue.mAllThreadList = factory.mThreadList;
+        cQueue.mAffineThreadLocal = cAffineThreadLocal;
+        prestartAllCoreThreads();
+    }
+
+    public void execute(final String tag, final Runnable command) {
+        cAffineThreadLocal.set(tag);
+        try {
+            execute(command);
+        } finally {
+            cAffineThreadLocal.remove();
+        }
+    }
+
+    protected static class ClusterQueue extends LinkedBlockingQueue<Runnable> {
+
+        public static final Random RANDOM = new Random();
+
+        static {
+            RANDOM.setSeed(System.currentTimeMillis());
+        }
+
+        public Multimap<String, Runnable> mRunnableMap = MultimapBuilder.treeKeys().linkedListValues().build();
+        public Multimap<Thread, String> mAffineThreadMap = MultimapBuilder.hashKeys().linkedListValues().build();
+        public Multimap<Thread, String> mAffineLockMap = MultimapBuilder.hashKeys().linkedListValues().build();
+        public List<Thread> mAllThreadList;
+        public ThreadLocal<String> mAffineThreadLocal;
+
+        @Override
+        public synchronized boolean offer(final Runnable runnable) {
+            String affine = mAffineThreadLocal.get();
+            Thread affineThread = null;
+            try {
+                if (Strings.isNullOrEmpty(affine)) {
+                    affine = DEFAULT_LOCK;
+                    affineThread = mAllThreadList.get(RANDOM.nextInt(mAllThreadList.size()));
+                    return super.offer(runnable);
+                } else {
+                    if (DEFAULT_LOCK.equals(affine)) {
+                        throw new IllegalArgumentException("thread tag cannot be" + DEFAULT_LOCK);
+                    }
+                    if (mAffineThreadMap.values().contains(affine)) {
+                        for (Thread item : mAffineThreadMap.keySet()) {
+                            if (mAffineThreadMap.get(item).contains(affine)) {
+                                affineThread = item;
+                                break;
+                            }
+                        }
+                    } else {
+                        Multiset<Thread> threadMultiset = HashMultiset.create();
+                        threadMultiset.addAll(mAllThreadList);
+                        for (Thread t : mAffineThreadMap.keySet()) {
+                            threadMultiset.add(t, mAffineThreadMap.get(t).size());
+                        }
+
+                        affineThread = new Ordering<Multiset.Entry<Thread>>() {
+                            @Override
+                            public int compare(final Multiset.Entry<Thread> left, final Multiset.Entry<Thread> right) {
+                                return Ints.compare(left.getCount(), right.getCount());
+                            }
+                        }.immutableSortedCopy(threadMultiset.entrySet())
+                                .get(0).getElement();
+                        mAffineThreadMap.put(affineThread, affine);
+                    }
+                    mRunnableMap.put(affine, runnable);
+                    return true;
+                }
+            } catch (Throwable e) {
+                throw new IllegalStateException(e);
+            } finally {
+                synchronized (this) {
+                    mAffineLockMap.put(affineThread, affine);
+                }
+                Preconditions.checkNotNull(affineThread);
+                synchronized (affineThread) {
+                    affineThread.notifyAll();
+                }
+            }
+        }
+
+        @Override
+        public synchronized int size() {
+            return super.size() + mRunnableMap.size();
+        }
+
+        @Override
+        public synchronized boolean isEmpty() {
+            return super.isEmpty() && mRunnableMap.isEmpty();
+        }
+
+        @Override
+        public synchronized boolean remove(final Object o) {
+            if (o == null) {
+                return false;
+            }
+            if (super.remove(o)) {
+                return true;
+            } else {
+                for (String key : mRunnableMap.keySet()) {
+                    if (mRunnableMap.get(key).remove(o)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        @Override
+        public Runnable poll(final long timeout, final TimeUnit unit) throws InterruptedException {
+            final Thread t = Thread.currentThread();
+            Runnable result = pollRunnableFromMap();
+            if (result != null) {
+                return result;
+            }
+            System.out.println("pool:" + t);
+            synchronized (t) {
+                t.wait(unit.toMillis(timeout), 0);
+            }
+            return pollRunnableFromMap();
+        }
+
+        @Override
+        public Runnable take() throws InterruptedException {
+            final Thread t = Thread.currentThread();
+            System.out.println("take:" + t);
+            Runnable result = pollRunnableFromMap();
+            if (result != null) {
+                return result;
+            }
+            synchronized (t) {
+                t.wait();
+            }
+            return pollRunnableFromMap();
+        }
+
+        protected synchronized Runnable pollRunnableFromMap() throws InterruptedException {
+            final Thread t = Thread.currentThread();
+            for (String affine : mAffineLockMap.get(t)) {
+                if (DEFAULT_LOCK.equals(affine)) {
+                    return super.take();
+                }
+                Runnable runnable = pollAffineRunnable(affine);
+                if (runnable != null) {
+                    return runnable;
+                }
+            }
+            return null;
+        }
+
+        protected Runnable pollAffineRunnable(String affine) {
+            if (mRunnableMap.get(affine).isEmpty()) {
+                return null;
+            }
+            final Iterator<Runnable> i = mRunnableMap.get(affine).iterator();
+            final Runnable runnable = i.next();
+            if (runnable == null) {
+                return null;
+            }
+            i.remove();
+            return runnable;
+        }
+    }
+
+    private class ClusterThreadFactor implements ThreadFactory {
+        private final AtomicInteger mCount = new AtomicInteger(0);
+        private ArrayList<Thread> mThreadList = Lists.newArrayList();
+
+        @Override
+        public Thread newThread(final Runnable r) {
+            Thread t = new Thread(r, "Nuts Task Thread #" + mCount.getAndIncrement());
+            mThreadList.add(t);
+            return t;
+        }
+    }
+
+
+}
---
+++ lib-common/src/test/java/io/demor/nuts/lib/ClusterExecutorTest.java
@@ -0,0 +1,86 @@
+package io.demor.nuts.lib;
+
+import com.google.common.collect.Maps;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ClusterExecutorTest {
+
+    SubClusterExecutor executor = new SubClusterExecutor(10, 20, 1, TimeUnit.SECONDS, new ThreadPoolExecutor.CallerRunsPolicy());
+
+    @Test
+    public void normal() throws Exception {
+        int jMax = 4;
+        int iMax = 500;
+        final CountDownLatch latch = new CountDownLatch(iMax * jMax);
+        for (int j = 0; j < jMax; ++j) {
+            for (int i = 0; i < iMax; ++i) {
+                final String tag = String.valueOf((char) ('a' + j));
+                executor.execute(tag, new TagRunnable(tag) {
+                    @Override
+                    public void run() {
+                        super.run();
+                        latch.countDown();
+                    }
+                });
+                executor.execute(new TagRunnable(null) {
+                    @Override
+                    public void run() {
+                        super.run();
+                        latch.countDown();
+                    }
+                });
+            }
+        }
+        latch.await(15, TimeUnit.SECONDS);
+        Assert.assertEquals(0, latch.getCount());
+    }
+
+    public static class TagRunnable implements Runnable {
+
+        public String tag;
+
+        public TagRunnable(final String tag) {
+            this.tag = tag;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("run:" + Thread.currentThread() + ";" + tag);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class SubClusterExecutor extends ClusterExecutor {
+
+        HashMap<String, Thread> map = Maps.newHashMap();
+
+        public SubClusterExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime, final TimeUnit unit, final RejectedExecutionHandler handler) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, handler);
+        }
+
+        @Override
+        protected void beforeExecute(final Thread t, final Runnable r) {
+            super.beforeExecute(t, r);
+            TagRunnable runnable = (TagRunnable) r;
+            if (runnable.tag == null) {
+                return;
+            }
+            if (map.containsKey(runnable.tag)) {
+                Assert.assertEquals(map.get(runnable.tag), t);
+            } else {
+                map.put(runnable.tag, t);
+            }
+        }
+    }
+}
